fc73ed1aa6981829faaed42a05c2ac1da2fb9fc5
Youngseok Choe <dev.youngseok@gmail.com>
2021-12-11 14:40:23 +0900

    Fix CommitDataset

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import pandas as pd
 import json
 from vocab import Vocab
+from easydict import EasyDict
 
 class CommitDataset(Dataset):
     def __init__(self, src_vocab: Vocab, trg_vocab: Vocab, file_path):
@@ -10,12 +11,15 @@ class CommitDataset(Dataset):
         self.df = pd.read_pickle(file_path)
     
     def __getitem__(self, index):
-        src = json.loads(self.df["commit_message"][index])
-        trg = json.loads(self.df["diff"][index])
-        src_ids = self.src_vocab.tokens2ids(src)
-        src_ids_ext = self.src_vocab.tokens2ids_ext(src)
-        trg_ids = self.trg_vocab.tokens2ids(trg)
-        return src_ids, src_ids_ext, trg_ids
+        item = EasyDict()
+        row = self.df.iloc[index]
+        src = json.loads(row["commit_messsage"])
+        trg = json.loads(row["diff"])
+        item.src_ids = self.src_vocab.tokens2ids(src)
+        item.src_ids_ext, item.oovs = self.src_vocab.tokens2ids_ext(src)
+        item.trg_ids = self.trg_vocab.tokens2ids(trg)
+        
+        return item
     
     def __len__(self):
         return len(self.df)
