662a177fae41bdb0874fcbebede91a45b352ce5f
Youngseok Choe <dev.youngseok@gmail.com>
2021-12-17 15:59:18 +0900

    Delete run.py

--- run.py
+++
@@ -1,4 +0,0 @@
-from test import test
-from train import train
-root = '/content/drive/MyDrive/CS492I/project'
-test(root,  f"{root}/final_models/seq2seqAttn_best.ckpt", use_pointer_gen=False, use_coverage=False)
